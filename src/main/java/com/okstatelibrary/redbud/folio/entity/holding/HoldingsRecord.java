1376ff187e594222ade20fdf3fec00aafb9ee825
Charmal Damith P <mcdperera@gmail.com>
2024-08-26 13:49:14 -0500

    Added gov doc update operation.

--- src/main/java/com/okstatelibrary/redbud/folio/entity/holding/HoldingsRecord.java
+++ src/main/java/com/okstatelibrary/redbud/folio/entity/holding/HoldingsRecord.java
@@ -3,4 +3,6 @@ package com.okstatelibrary.redbud.folio.entity.holding;
 public class HoldingsRecord {
 	public String instanceId;
 	public boolean discoverySuppress;
+	public String permanentLocationId;
+	public String effectiveLocationId;
 }
